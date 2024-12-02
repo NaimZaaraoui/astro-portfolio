08cd2edefd7fdd4db61ac3c3d07ce889c0051159
ghostcode <ghostcode1999@gmail.com>
2024-12-02 09:43:26 +0100

    Change rendering mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,6 @@ export default defineConfig({
   },
   site: "https://stargazers.club",
   integrations: [icon(), sitemap(), react()],
-  // output: "hybrid",
-  // adapter: netlify()
+  output: "hybrid",
+  adapter: netlify()
 });
